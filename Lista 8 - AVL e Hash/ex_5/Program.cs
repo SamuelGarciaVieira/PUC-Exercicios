9db0272860b3aa0bf8e3f80c62a9d9f37ed3f8d2
Samuel Garcia <samuelportovieira@gmail.com>
2024-12-03 14:52:35 -0300

    Lista 8 - Ex_5
    Implementação inicial

---
+++ Lista 8 - AVL e Hash/ex_5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ex_5
+{
+    class Celula
+    {
+        private int matricula;
+        private string nome;
+        private string curso;
+        private Celula prox;
+        public Celula(int matricula, string nome, string curso)
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+            this.curso = curso;
+            this.prox = null;
+        }
+        public Celula()
+        {
+            this.matricula = 0;
+            this.nome = "";
+            this.curso = "";
+            this.prox = null;
+        }
+        public Celula Prox
+        {
+            get { return prox; }
+            set { prox = value; }
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Curso
+        {
+            get { return curso; }
+            set { curso = value; }
+        }
+    }
+    class Lista
+    {
+        private Celula primeiro, ultimo;
+        public Lista()
+        {
+            primeiro = new Celula();
+            ultimo = primeiro;
+        }
+    }
+    class Hash
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[args])
+        {
+
+        }
+    }
+}
